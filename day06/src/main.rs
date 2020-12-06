588616a6d43433ac5221971e0871da3267078a0f
Sean Chen <skypemaster007@gmail.com>
2020-12-06 17:01:34 -0600

    Add day 6 part II

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,13 +1,28 @@
 use aocf::Aoc;
+use counter::Counter;
 use std::collections::HashSet;
 
 fn part_one(input: &str) -> usize {
     input.split("\n\n")
-        .fold(0, |acc, line| {
-            acc + line.chars()
+        .fold(0, |acc, group| {
+            acc + group.chars()
                 .filter(|c| *c != '\n')
                 .collect::<HashSet<char>>()
                 .len()
+        })
+}
+
+fn part_two(input: &str) -> usize {
+    input.split("\n\n")
+        .fold(0, |acc, group| {
+            let nlines = group.split('\n').count();
+            let char_counts = group.split('\n')
+                .flat_map(|s| s.chars())
+                .collect::<Counter<_>>();
+
+            acc + char_counts.values()
+                .filter(|v| **v == nlines)
+                .count()
         })
 }
 
@@ -19,7 +34,10 @@ fn main() {
         .unwrap();
 
     if let Ok(input) = aoc.get_input(false) {
-        let answer = part_one(&input);
+        let answer = part_one(&input.trim());
+        println!("{}", answer);
+
+        let answer = part_two(&input.trim());
         println!("{}", answer);
     }
 }
@@ -40,6 +58,32 @@ a
 a
 a
 a
+
 b";
     assert_eq!(part_one(input), 11);
+}
+
+#[test]
+fn test_part_two() {
+    let input = "abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b";
+    assert_eq!(part_two(input), 6);
+
+    let input = "kn
+nk
+nuk";
+    assert_eq!(part_two(input), 2);
 }
