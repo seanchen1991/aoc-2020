4263edbcfaf3411c9dbd82b5cf7024cde42cf696
Sean Chen <skypemaster007@gmail.com>
2020-12-09 18:42:21 -0600

    Add day 9 part II

--- day09/src/lib.rs
+++ day09/src/lib.rs
@@ -1,19 +1,42 @@
+use std::cmp::Ordering;
 use std::iter::FromIterator;
 use std::collections::HashSet;
 
 const PREAMBLE_LENGTH: usize = 25;
 
-pub fn part_one(input: &str) -> i64 {
-    let nums: Vec<_> = input.lines()
-        .map(|num| num.parse::<i64>().expect("Failed to parse a line of input"))
-        .collect();
-
+pub fn part_one(nums: &[i64]) -> i64 {
     (PREAMBLE_LENGTH..nums.len()).find(|i| {
         let preamble: HashSet<&i64> = HashSet::from_iter(nums[i - PREAMBLE_LENGTH..*i].iter());
         preamble.iter().map(|num| nums[*i as usize] - *num).all(|diff| !preamble.contains(&diff))
     })
     .map(|i| nums[i])
     .expect("No answer found")
+}
+
+pub fn part_two(nums: &[i64], target: i64) -> i64 {
+    let (mut i, mut j, mut sum) = (0, 0, 0);
+
+    loop {
+        match sum.cmp(&target) {
+            Ordering::Greater => {
+                sum -= nums[usize::min(i, nums.len() - 1)];
+                i += 1;
+            },
+            Ordering::Less => {
+                sum += nums[usize::min(j, nums.len() - 1)];
+                j += 1;
+            },
+            Ordering::Equal => {
+                let min = nums[i..j].iter()
+                    .min()
+                    .expect("No min found");
+                let max = nums[i..j].iter()
+                    .max()
+                    .expect("No max found");
+                return min + max;
+            }
+        }
+    }
 }
 
 #[cfg(test)]
--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,5 +1,5 @@
 use aocf::Aoc;
-use day09::part_one;
+use day09::{part_one, part_two};
 
 fn main() {
     let mut aoc = Aoc::new()
@@ -9,7 +9,14 @@ fn main() {
         .unwrap();
 
     if let Ok(input) = aoc.get_input(false) {
-        let answer = part_one(&input);
+        let nums: Vec<i64> = input.lines()
+            .map(|n| n.parse::<i64>().expect("Failed to parse a line of input"))
+            .collect();
+
+        let answer = part_one(&nums);
+        println!("{}", answer);
+
+        let answer = part_two(&nums, answer);
         println!("{}", answer);
     }
 }
